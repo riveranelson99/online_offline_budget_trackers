15065ead28ea289f91dcba6dabb56cbc40679788
Nelson Rivera <riveranelson99@gmail.com>
2021-11-23 09:45:08 -0600

    Adjusted typos and adjusted intended functionality of app

--- public/db.js
+++ public/db.js
@@ -1,8 +1,7 @@
 let db;
 let budgetVersion;
 
-// Create a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+const request = indexedDB.open('BudgetDB', budgetVersion || 1);
 
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
@@ -24,27 +23,15 @@ request.onerror = function (e) {
 };
 
 function checkDatabase() {
-  // Sanity check
   console.log('check db invoked');
 
-  // Open a transaction on your BudgetStore db
-  // Establish connection with db for read write access
   let transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // access your BudgetStore object
-  // specify the specific object store you wish to access
   const store = transaction.objectStore('BudgetStore');
 
-  // Get all records from store and set to a variable
-  // Get all data from the previously specified object store
   const getAll = store.getAll();
 
-  // If the request was successful
-  // Define what to do next if all previous methods were successful
   getAll.onsuccess = function () {
-    // If there are items in the store, we need to bulk add them when we are back online
-    // Establish if condition that returns true if the array data is greater than the length of 0
-    // If the value of true is returned, make a post request
     if (getAll.result.length > 0) {
       fetch('/api/transaction/bulk', {
         method: 'POST',
@@ -56,19 +43,11 @@ function checkDatabase() {
       })
         .then((response) => response.json())
         .then((res) => {
-          // If our returned response is not empty
-          // Establish an if condition based on the response from the post request if the response data is not 0
           if (res.length !== 0) {
-            // Open another transaction to BudgetStore with the ability to read and write
-            // Establish a connection to the database for read write access
             transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-            // Assign the current store to a variable
-            // Specify the exact object store you are wanting to read write to
             const currentStore = transaction.objectStore('BudgetStore');
 
-            // Clear existing entries because our bulk add was successful
-            // Clear store data as it has been added to the server db and we do not wish to duplicate this data
             currentStore.clear();
             console.log('Clearing store 🧹');
           }
@@ -81,7 +60,6 @@ request.onsuccess = function (e) {
   console.log('success');
   db = e.target.result;
 
-  // Check if app is online before reading from db
   if (navigator.onLine) {
     console.log('Backend online! 🗄️');
     checkDatabase();
@@ -89,20 +67,13 @@ request.onsuccess = function (e) {
 };
 
 const saveRecord = (record) => {
-  // console log a success message to know function worked as intended
   console.log('Save record invoked');
-  // Create a transaction on the BudgetStore db with readwrite access
-  // establish a connection with the BudgetStore database for the purposes of read write access
+
   const transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // Access your BudgetStore object store
-  // Access the specific object store you are attempting to target
   const store = transaction.objectStore('BudgetStore');
 
-  // Add record to your store with add method.
-  // Add the data to the object store
   store.add(record);
 };
 
-// Listen for app coming back online
 window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,34 @@
 // TODO: add listener and handler to cache static assets
 // TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser 
-
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/db.js",
   "/index.html",
   "/index.js",
-  "/style.css",
-  "icons/icon-192x192.png",
-  "icons/icon-512x512.png",
+  "/db.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
   "/manifest.webmanifest",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 // install
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
+//activate
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
@@ -46,14 +46,12 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
+self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -61,7 +59,6 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
@@ -70,11 +67,11 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
 
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
